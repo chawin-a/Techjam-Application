a923c592b03ccccef7b360e86847e5e73f9f169f
Chawin Aiemvaravutigul <nick@Cint-2.local>
2019-11-30 18:18:28 +0700

    add features

--- webapp/services.py
+++ webapp/services.py
@@ -5,13 +5,39 @@ from http import HTTPStatus
 from flask import Flask, jsonify, request
 import math
 
+robot_re = re.compile(r"^robot#([1-9][0-9]*)$")
+robots = {}
+
 app = Flask(__name__)
-def distance(pos1, pos2, metric):
-    if metric == "manhattan":
-        p = 1
-    elif metric == "euclidean":
-        p = 2
-    return math.pow(math.pow(pos1["x"] - pos2["x"], p) + math.pow(pos1["y"] - pos2["y"], p),(1/p))
+def distance(pos1, pos2, metric="euclidean"):
+    try:
+        print(pos1)
+        print(pos2)
+        if metric == "manhattan":
+            p = 1
+        elif metric == "euclidean":
+            p = 2
+        else:
+            raise Exception("Error")
+        return math.pow(math.pow(pos1["x"] - pos2["x"], p) + math.pow(pos1["y"] - pos2["y"], p),(1/p))
+    except:
+        raise Exception("Error")
+
+def _get_position(obj):
+    if 'x' in obj and 'y' in obj:
+        return obj
+    elif isinstance(obj, str):
+        if robot_re.match(obj):
+            _id = int(obj.split('#')[1])
+            if _id in robots:
+                return robots[_id]
+            else:
+                raise Exception("Error")
+        else:
+            raise Exception("Error")
+    else:
+        raise Exception("Error")
+
 
 @app.route("/distance", methods=['POST'])
 def calculate_distance():
@@ -21,16 +47,18 @@ def calculate_distance():
         return '', HTTPStatus.BAD_REQUEST
     if 'metric' in body:
         met = body['metric']
-    pos1 = body['first_pos']
-    pos2 = body['second_pos']
-    result = distance(pos1, pos2, met)
+    try:
+        pos1 = _get_position(body['first_pos'])
+        pos2 = _get_position(body['second_pos'])
+    except:
+        return '', HTTPStatus.BAD_REQUEST
+
+    try:
+        result = distance(pos1, pos2, met)
+    except:
+        return '', HTTPStatus.FAILED_DEPENDENCY
     result = f"{result:.3f}"
     return jsonify(distance=result), HTTPStatus.OK
-
-
-robot_re = re.compile(r"^robot#([1-9][0-9]*)$")
-
-robots = {}
 
 @app.route("/robot/<robot_id>/position", methods=['PUT'])
 def update_position(robot_id):
@@ -40,17 +68,17 @@ def update_position(robot_id):
     except:
         return '', HTTPStatus.BAD_REQUEST
 
-    if _id >= 1 and _id <= 999999:
+    if _id < 1 or _id > 999999:
         return '', HTTPStatus.BAD_REQUEST
 
-    if _id in robots:
-        status = HTTPStatus.NO_CONTENT  # 204
-    else:
-        status = HTTPStatus.CREATED  # 201
+    # if _id in robots:
+    #     status = HTTPStatus.NO_CONTENT  # 204
+    # else:
+    #     status = HTTPStatus.CREATED  # 201
 
-    robots[_id] = {"position":body['position']}
+    robots[_id] = body['position']
 
-    return '', HTTPStatus.OK
+    return '', HTTPStatus.NO_CONTENT
 
 @app.route("/robot/<robot_id>/position", methods=['GET'])
 def get_position(robot_id):
@@ -60,13 +88,43 @@ def get_position(robot_id):
     except:
         return '', HTTPStatus.BAD_REQUEST
     
-    if _id >= 1 and _id <= 999999:
+    if _id < 1 or _id > 999999:
         return '', HTTPStatus.BAD_REQUEST
 
     if _id not in robots:
         return '', HTTPStatus.NOT_FOUND
 
-    return jsonify(robots[_id]), HTTPStatus.OK
+    return jsonify(position=robots[_id]), HTTPStatus.OK
+
+
+@app.route("/nearest", methods=['POST'])
+def get_nearest():
+    body = request.get_json()
+
+    if "ref_position" not in body:
+        return '', HTTPStatus.BAD_REQUEST
+    ref = body["ref_position"]
+    print(ref)
+    try:
+        result = []
+        dis = 999999999999
+        print("esfjlsdf")
+        print(robots)
+        for robot, position in robots.items():
+            print(robot)
+            print(position)
+            d = distance(position, ref)
+            if dis > d + 1e-9:
+                dis = d
+                result = []
+            if d >= dis - 1e-9 and d <= dis + 1e-9:
+                result.append(robot)
+    except:
+        return '', HTTPStatus.BAD_REQUEST
+
+    return jsonify(robot_ids=result), HTTPStatus.OK
+
+
 
 # @app.route("/robot/<robot_id>/position", methods='PUT')
 # def update_position(robot_id):
